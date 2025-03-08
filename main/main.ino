8ead6fba1c73f55f81f612f9bce49ba00803ed86
Patrick Ryan <pat.m.ryan@gmail.com>
2025-03-07 20:50:47 -0500

    Updates to arduino

--- main/main.ino
+++ main/main.ino
@@ -1,100 +1,192 @@
 #include <DFPlayerMini_Fast.h>
 #include "sbus.h"
 #include <SoftwareSerial.h>
-#include <Adafruit_NeoPixel.h>
+#include "CountDown.h"
 
-// Configuration
+// SBUS RC values
+constexpr int RC_HIGH = 1811;
+constexpr int RC_MID = 992;
+constexpr int RC_LOW = 172;
 
-#define RC_HIGH 1811
-#define RC_LOW 172
+// Channels (Start at 0, on Transmitter will start at 1)
+constexpr int CH_UPPER_LIFT = 5;
+constexpr int CH_LOWER_LIFT = 6;
+constexpr int CH_DATA_RINGS = 14;
+constexpr int CH_MP3_TRIG = 15;
 
-#define SBUS_RX 16
-#define SBUS_TX 17
+// SBUS communication pins
+constexpr int SBUS_RX = 16;
+constexpr int SBUS_TX = 17;
 
-#define MP3_RX 18
-#define MP3_TX 19
-#define MP3_BUSY 23
-#define MP3_CHANNEL 15
-#define MP3_NUM_FILES 4
+// DFPlayer module
+constexpr int MP3_RX = 18;
+constexpr int MP3_TX = 19;
+constexpr int MP3_BUSY = 23;
 
-#define PIXEL_PIN 5
-#define PIXEL_COUNT 1
+// Data ring motor control pins
+constexpr int M1 = 32;
+constexpr int E1 = 33;
+constexpr int M2 = 25;
+constexpr int E2 = 26;
+constexpr int DATARING_SPEED = 110;
 
-#define M1 32
-#define E1 33
-#define M2 25
-#define E2 26
+CountDown bodyLiftCountdown;
+CountDown e1Countdown;
+CountDown e2Countdown;
+
+bool e1Direction = LOW;
+int e1Speed = 0;
+bool e2Direction = LOW;
+int e2Speed = 0;
 
 bfs::SbusRx sbus_rx(&Serial1, SBUS_RX, SBUS_TX, true);
 bfs::SbusData data;
 
 SoftwareSerial mySerial(MP3_RX, MP3_TX);
-
-Adafruit_NeoPixel pixels(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
 DFPlayerMini_Fast dfPlayer;
+
+int prevLowerLift;
+int prevUpperLift;
+bool bodyLiftInProgress = false;
 
 void setup()
 {
-  Serial.begin(115200);
+    pinMode(MP3_BUSY, INPUT);
+    pinMode(M1, OUTPUT);
+    pinMode(E1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    pinMode(E2, OUTPUT);
 
-  // dfPlayer Busy Signal
-  pinMode(MP3_BUSY, INPUT);
+    mySerial.begin(9600);
+    dfPlayer.begin(mySerial, true);
+    dfPlayer.volume(15);
 
-  // Data Ring Motors
-  pinMode(M1, OUTPUT);
-  pinMode(E1, OUTPUT);
-  pinMode(M2, OUTPUT);
-  pinMode(E2, OUTPUT);
-
-  // Neopixels
-  pixels.begin();
-
-  // DF Player
-  mySerial.begin(9600);
-  dfPlayer.begin(mySerial, true);
-  dfPlayer.volume(30);
-  dfPlayer.play(1);
-
-  // SBUS
-  sbus_rx.Begin();
+    sbus_rx.Begin();
+    if (sbus_rx.Read())
+    {
+        data = sbus_rx.data();
+        prevLowerLift = data.ch[CH_LOWER_LIFT];
+        prevUpperLift = data.ch[CH_UPPER_LIFT];
+    }
 }
 
 void loop()
 {
-  if (sbus_rx.Read()) {
-    data = sbus_rx.data();
-    audioTrigger();
-    dataRingMotors();
-  }
+    if (sbus_rx.Read())
+    {
+        data = sbus_rx.data();
+        playTriggeredSound();
+        controlDataRingMotors();
+    }
 }
 
-/**
-   Spin the internal data rings
-*/
-void dataRingMotors()
+void stopDataRings()
 {
-  int motorSpeed = map(data.ch[13], RC_LOW, RC_HIGH, -255, 255);
-
-  if (motorSpeed < 0)
-  {
-    digitalWrite(M1, LOW);
-  }
-  else
-  {
-    digitalWrite(M1, HIGH);
-  }
-
-  analogWrite(E1, abs(motorSpeed));
+    e1Speed = 0;
+    e1Countdown.stop();
+    e2Speed = 0;
+    e2Countdown.stop();
+    bodyLiftCountdown.stop();
+    updateMotor(M1, E1, e1Direction, e1Speed);
+    updateMotor(M2, E2, e2Direction, e2Speed);
 }
 
-void audioTrigger()
+void controlDataRingMotors()
 {
-  int soundIndex = getSoundIndex(data.ch[MP3_CHANNEL]);
+    if (isBodyLiftActive() && !bodyLiftInProgress)
+    {
+        e1Direction = LOW;
+        e1Speed = DATARING_SPEED;
+        e1Countdown.stop();
+        e2Direction = LOW;
+        e2Speed = DATARING_SPEED;
+        e2Countdown.stop();
+        bodyLiftInProgress = true;
+    }
+    else if (!isBodyLiftActive() && bodyLiftInProgress)
+    {
+        stopDataRings();
+        bodyLiftInProgress = false;
+    }
+    else if (areDataRingsActive())
+    {
+        int spinLow = 150, spinHigh = 750, delayLow = 800, delayHigh = 2500;
+        if (data.ch[CH_DATA_RINGS] == RC_MID)
+        {
+            spinLow = 100;
+            spinHigh = 400;
+            delayLow = 500;
+            delayHigh = 500;
+        }
 
-  // Check dfPlayer status
-  if (soundIndex > 0 && digitalRead(MP3_BUSY) == HIGH)
-  {
-    dfPlayer.play(soundIndex);
-  }
+        if (e1Countdown.isStopped() && e1Speed == 0)
+        {
+            e1Countdown.start(random(spinLow, spinHigh));
+            e1Direction = random(0, 2);
+            e1Speed = DATARING_SPEED;
+        }
+        if (e2Countdown.isStopped() && e2Speed == 0)
+        {
+            e2Countdown.start(random(spinLow, spinHigh));
+            e2Direction = random(0, 2);
+            e2Speed = DATARING_SPEED;
+        }
+        if (e1Countdown.isStopped() && e1Speed == DATARING_SPEED)
+        {
+            e1Speed = 0;
+            e1Countdown.start(random(delayLow, delayHigh));
+        }
+        if (e2Countdown.isStopped() && e2Speed == DATARING_SPEED)
+        {
+            e2Speed = 0;
+            e2Countdown.start(random(delayLow, delayHigh));
+        }
+    }
+    else if (!bodyLiftInProgress)
+    {
+        stopDataRings();
+    }
+
+    updateMotor(M1, E1, e1Direction, e1Speed);
+    updateMotor(M2, E2, e2Direction, e2Speed);
+}
+
+void playTriggeredSound()
+{
+    int soundIndex = getSoundIndex(data.ch[CH_MP3_TRIG]);
+    if (soundIndex > 0)
+        dfPlayer.play(soundIndex);
+}
+
+bool isBodyLiftActive()
+{
+    if (data.ch[CH_LOWER_LIFT] != prevLowerLift || data.ch[CH_UPPER_LIFT] != prevUpperLift)
+    {
+        if (bodyLiftCountdown.isStopped())
+        {
+            bodyLiftCountdown.start(250);
+            prevLowerLift = data.ch[CH_LOWER_LIFT];
+            prevUpperLift = data.ch[CH_UPPER_LIFT];
+            return true;
+        }
+    }
+    return bodyLiftCountdown.remaining() > 0;
+}
+
+bool areDataRingsActive()
+{
+    return data.ch[CH_DATA_RINGS] == RC_LOW || data.ch[CH_DATA_RINGS] == RC_MID;
+}
+
+int getSoundIndex(int triggerValue)
+{
+    if (triggerValue < 172 || triggerValue > 1680)
+        return -1;
+    return 10 - (triggerValue - 172) / 180;
+}
+
+void updateMotor(int motor, int enable, bool direction, int speed)
+{
+    digitalWrite(motor, direction);
+    analogWrite(enable, speed);
 }
